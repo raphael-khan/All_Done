12203858369666489b8294f9868e53b7a243e5a9
raphael-khan <raphaelkhan@yahoo.com>
2022-04-09 17:33:26 -0400

    created the toggle reminder prop

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
 
+
 const App = () => {
   const [tasks, setTasks] = useState([
     {
@@ -24,13 +25,34 @@ const App = () => {
         reminder: true,
     }
 ])
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  } 
+  // Toggle Reminder 
+  
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder: 
+        !task.reminder } : task
+        )
+      )
+  }
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      ) : (
+        <p className='task'>Great Work. You're ALL DONE !'</p>
+      )
+      }
     </div>
   )
 }
+
 
 export default App
 
