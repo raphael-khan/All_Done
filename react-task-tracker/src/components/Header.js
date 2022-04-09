e8990c9a00c3a9eafc7402981ab5569a5713c4ca
raphael-khan <raphaelkhan@yahoo.com>
2022-04-09 14:15:57 -0400

    created header and button components

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,23 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+const Header = ({title}) => {
+    const onClick = () => {
+        console.log('click')
+    }
     return (
-        <div>
-            <h1>Task Tracker</h1>
+        <div className='header'>
+            <h1>{title}</h1>
+            <Button color='orange' text='Add Task' onClick={onClick}/>
         </div>
     )
 }
 
 export default Header
+
+Header.defaultProps = {
+    title: 'ALL DONE - Task Tracker'
+}
+
+Header.prototype = {
+    title: PropTypes.string.isRequired
+}
