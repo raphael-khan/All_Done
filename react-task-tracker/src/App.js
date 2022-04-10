571ad5b20b4f15ca8edd9888885b63816a5a85dc
raphael-khan <raphaelkhan@yahoo.com>
2022-04-09 22:47:12 -0400

    created AddTask and set up state'

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 
@@ -43,6 +44,7 @@ const App = () => {
   return (
     <div className='container'>
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
       ) : (
