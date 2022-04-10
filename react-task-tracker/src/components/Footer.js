68f5abd2ec3f988ec44d6ac12e10957ebb082289
raphael-khan <raphaelkhan@yahoo.com>
2022-04-10 16:21:02 -0400

    created footer component

---
+++ react-task-tracker/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <h5>Copyright &copy; 2022</h5>
+        <a href='/about'>About</a>
+    </div>
+  )
+}
+
+export default Footer
